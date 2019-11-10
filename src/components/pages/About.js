bb78162e0e6133d21ffcec3ec8b90c671008be36
orbena <or.b@taboola.com>
2019-11-10 17:47:27 +0200

    Course finished - play time

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+function About() {
+     return (
+         <React.Fragment>
+             <h1>About</h1>
+             <p>This is the TODO list App V 1.0.0 madafaka</p>
+         </React.Fragment>
+     )
+ }
+ 
+ export default About;

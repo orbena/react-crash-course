d407acec1692763470430320870058eb52d9fe8b
orbena <or.b@taboola.com>
2019-11-14 13:41:23 +0200

    added Form component

---
+++ src/components/layout/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Form</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Form
